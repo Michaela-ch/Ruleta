efd55c8a643b61604524ba63a2ef2bd1c0aae7a9
Michaela Chlubna <michaela.chlubna@seznam.cz>
2025-03-02 11:54:34 +0100

    aktualizace

--- index.js
+++ index.js
@@ -5,20 +5,56 @@ if (number===0) {
 } else if (number>36 || number<0) {
         document.body.innerHTML += 'číslo je moc velké nebo záporné'}
     else {
-        if (number%2===0) {
-            document.body.innerHTML += 'číslo je sudé, '
+        if ((number>=1 && number <= 10) || (number >=19 && number <= 28)) {
 
-            if ((number>=1 && number <= 10) || (number >=19 && number <= 28)) {
-                document.body.innerHTML += 'černá'
-            } else if ((number>=11 && number <= 18) || (number >=29 && number <= 36)){
-                    document.body.innerHTML += 'červená'
+            if (number%2===0) {
+                document.body.innerHTML += 'číslo je sudé, černá'
+            }else {
+                document.body.innerHTML += 'číslo je liché, červená'
             }
+        } else if ((number>=11 && number <= 18) || (number >=29 && number <= 36)){
+                    
+            if (number%2===0) {
+                document.body.innerHTML += 'číslo je sudé, červená'
+            }
+            else {document.body.innerHTML += 'číslo je liché, černá'}}}
+       
 
-        } else {
-            document.body.innerHTML += 'číslo je liché, '
 
-            if ((number>=1 && number <= 10) || ( number >=19 && number <= 28)) {
-                document.body.innerHTML += 'červená'
-            } else if ((number>=11 && number <= 18) || (number >=29 && number <= 36)){
-                document.body.innerHTML+= 'černá'}}}
+
+
+
+
+
+
+
+
+
+
+//puvodni
+
+
+// const number = Number(prompt("Zadejte číslo od 0 do 36"));
+
+// if (number===0) {
+//     document.body.innerHTML += 'číslo je 0, zelená'
+// } else if (number>36 || number<0) {
+//         document.body.innerHTML += 'číslo je moc velké nebo záporné'}
+//     else {
+//         if (number%2===0) {
+//             document.body.innerHTML += 'číslo je sudé, '
+
+//             if ((number>=1 && number <= 10) || (number >=19 && number <= 28)) {
+//                 document.body.innerHTML += 'černá'
+//             } else if ((number>=11 && number <= 18) || (number >=29 && number <= 36)){
+//                     document.body.innerHTML += 'červená'
+//             }
+
+//         } else {
+//             document.body.innerHTML += 'číslo je liché, '
+
+//             if ((number>=1 && number <= 10) || ( number >=19 && number <= 28)) {
+//                 document.body.innerHTML += 'červená'
+//             } else if ((number>=11 && number <= 18) || (number >=29 && number <= 36)){
+//                 document.body.innerHTML+= 'černá'}}}
 
